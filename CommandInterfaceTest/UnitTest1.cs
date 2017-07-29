260a4bfa097b6aa5bbc0b66e5523b669f9369040
hakomikan <mail@s-taki.sakura.ne.jp>
2017-07-29 18:25:03 +0900

    wip

---
+++ CommandInterfaceTest/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+
+namespace CommandInterfaceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            Assert.AreEqual(await CSharpScript.EvaluateAsync<int>("1 + 2"), 3);
+        }
+
+        [TestMethod]
+        public void RunAsScript()
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(@"using System;
+using System.Collections.Generic;
+
+namespace CommandInterfaceTest
+{
+    public class TestScript
+    {
+        public static int Main()
+        {
+            return 0;
+        }
+    }
+}
+");
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                "assemblyName",
+                new[] { syntaxTree },
+                new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var dll = new MemoryStream())
+            using (var pdb = new MemoryStream())
+            {
+                var emitResult = compilation.Emit(dll, pdb);
+                if (emitResult.Success)
+                {
+                    var assembly = Assembly.Load(dll.ToArray(), pdb.ToArray());
+                    var types = assembly.GetTypes();
+                    var targetClass = types[0];
+                    var targetMethods = targetClass.GetMethods();
+                    var methodBase = targetClass.GetMethod("Main", BindingFlags.Static | BindingFlags.Public);
+
+                    Assert.AreEqual(0, methodBase.Invoke(null, null));
+                }
+                else
+                {
+                    throw new Exception("ahhh");
+                }
+            }
+        }
+    }
+}

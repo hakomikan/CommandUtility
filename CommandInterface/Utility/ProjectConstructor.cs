a5d40f001cecccfc9102f77f8f9f21ec4ede77ed
hakomikan <mail@s-taki.sakura.ne.jp>
2017-09-17 10:51:30 +0900

    add utilities of project and solution

---
+++ CommandInterface/Utility/ProjectConstructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandInterface.Utility
+{
+    public class ProjectConstructor
+    {
+        public ProjectConstructor()
+        {
+
+        }
+
+        public string ProjectPath { get; set; }
+
+        public void CreateProject()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommandInterfaceTest/Utility/ProjectConstructorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommandInterface.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandInterface;
+using System.IO;
+
+namespace CommandInterface.Utility.Tests
+{
+    [TestClass()]
+    public class ProjectConstructorTests
+    {
+        [TestMethod()]
+        public void ProjectConstructorTest()
+        {
+            var constructor = new ProjectConstructor();
+
+            // プロジェクトを生成する
+            constructor.CreateProject();
+
+            // 生成されたプロジェクトファイルを確認する
+            Assert.IsTrue(File.Exists(constructor.ProjectPath));            
+        }
+    }
+}

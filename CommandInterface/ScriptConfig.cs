5ae7d1fc521231ef3636ebf4c84ecf780570521f
hakomikan <mail@s-taki.sakura.ne.jp>
2017-10-07 20:04:29 +0900

    add config loader(empty)

---
+++ CommandInterface/ScriptConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CommandInterface
+{
+    public class ScriptConfig
+    {
+        public string ScriptPath { get; set; }
+        public string SolutionPath { get; set; }
+        public string ProjectPath { get; set; }
+
+        public ScriptConfig Load(FileInfo configFile)
+        {
+
+        }
+    }
+}
---
+++ CommandInterfaceTest/Configs/TestConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CommandInterface.Configs.Test
+{
+    public class Config : ScriptConfig
+    {
+        public Config()
+        {
+            SolutionPath = "testSolutionPath";
+            ScriptPath = "testScriptPath";
+            ProjectPath = "testProjectPath";
+        }
+    }
+}

47eeb4d4559e4a10ed2b4705832715cb49c64f0b
hakomikan <mail@s-taki.sakura.ne.jp>
2017-10-08 12:30:30 +0900

    try a config expressions

--- CommandInterface/ScriptConfig.cs
+++ CommandInterface/ScriptConfig.cs
@@ -7,15 +7,50 @@ using System.IO;
 
 namespace CommandInterface
 {
-    public class ScriptConfig
+    public abstract class BaseConfig
     {
-        public string ScriptPath { get; set; }
-        public string SolutionPath { get; set; }
-        public string ProjectPath { get; set; }
-
-        public ScriptConfig Load(FileInfo configFile)
+        /// <summary>
+        /// スクリプトの探索ディレクトリ
+        /// 以下のように定義します。
+        ///     public override string => ProjectRoot.Combine("xxxxx");
+        /// </summary>
+        public virtual string ScriptPath { get; }
+        /// <summary>
+        /// ソリューションを生成するときの場所
+        /// </summary>
+        public abstract string SolutionPath
         {
+            get;
+        }
+        /// <summary>
+        /// プロジェクトファイルを生成する時の場所
+        /// </summary>
+        public abstract string ProjectPath { get; }
 
+        public string SystemRoot = "xxxxx";
+
+        public BaseConfig Load(FileInfo configFile)
+        {
+            return null;
+        }
+    }
+
+    public class MyConfig : BaseConfig
+    {
+        public override string ProjectPath => SystemRoot + "aaa";
+
+        public override string ScriptPath
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public override string SolutionPath => "";
+
+        public Asa()
+        {
+            this.Sci
         }
     }
 }

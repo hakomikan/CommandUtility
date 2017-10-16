615a7ec1c92a50fc12dd838550dd1a9982219036
hakomikan <mail@s-taki.sakura.ne.jp>
2017-10-16 23:13:06 +0900

    add directoryinfo utility

--- CommandInterface/Utility/FileUtility.cs
+++ CommandInterface/Utility/FileUtility.cs
@@ -44,4 +44,17 @@ namespace CommandInterface.Utility
             return Uri.UnescapeDataString(baseUri.MakeRelativeUri(pathUri).ToString().Replace('/', Path.DirectorySeparatorChar));
         }
     }
+
+    public static class DirectoryInfoExtensions
+    {
+        public static FileInfo CombineAsFile(this DirectoryInfo directoryInfo, string filename)
+        {
+            return new FileInfo(Path.Combine(directoryInfo.FullName, filename));
+        }
+
+        public static DirectoryInfo CombineAsDirectory(this DirectoryInfo directoryInfo, string directoryName)
+        {
+            return new DirectoryInfo(Path.Combine(directoryInfo.FullName, directoryName));
+        }
+    }
 }

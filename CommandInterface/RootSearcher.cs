35a123df17d53e0db094b3938b8f77a1ee17019d
hakomikan <mail@s-taki.sakura.ne.jp>
2017-10-20 22:17:10 +0900

    add root searcher implements

--- CommandInterface/RootSearcher.cs
+++ CommandInterface/RootSearcher.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandInterface.Utility;
 
 namespace CommandInterface
 {
@@ -19,7 +20,7 @@ namespace CommandInterface
 
     public abstract class ISearcher
     {
-
+        public abstract IEnumerable<DirectoryInfo> Directories { get; }
     }
 
     public class ApplicationRelativeSearcher : ISearcher
@@ -29,6 +30,14 @@ namespace CommandInterface
         public ApplicationRelativeSearcher(DirectoryInfo appDir)
         {
             this.ApplicationDirectory = appDir;
+        }
+
+        public override IEnumerable<DirectoryInfo> Directories
+        {
+            get
+            {
+                yield return ApplicationDirectory;
+            }
         }
     }
 
@@ -42,28 +51,72 @@ namespace CommandInterface
             this.CurrentDirectory = currentDirectory;
             this.RootFileName = rootFileName;
         }
+
+        public override IEnumerable<DirectoryInfo> Directories
+        {
+            get
+            {
+                for (var currentDirectory = CurrentDirectory;
+                    currentDirectory != null;
+                    currentDirectory = currentDirectory.Parent)
+                {
+                    var rootFilePath = currentDirectory.CombineAsFile(RootFileName);
+                    if (rootFilePath.Exists)
+                    {
+                        yield return currentDirectory;
+                    }
+                }
+            }
+        }
     }
 
     public class EnvironmentVariableSearcher : ISearcher
     {
         public EnvironmentVariableSearcher(string name, IEnvironmentVariableAccessor envAccessor)
         {
+            Name = name;
             EnvironmentVariables = envAccessor;
         }
 
+        private string Name { get; set; }
         private IEnvironmentVariableAccessor EnvironmentVariables { get; set; }
+
+        public override IEnumerable<DirectoryInfo> Directories
+        {
+            get
+            {
+                foreach (var directoryName in EnvironmentVariables.Get(Name).Split(';'))
+                {
+                    yield return new DirectoryInfo(directoryName);
+                }
+            }
+        }
     }
 
-    public class RootSearcher
+    public class RootSearcher : ISearcher
     {
         public RootSearcher(params ISearcher[] searchers)
         {
             Searchers = searchers.ToList();
         }
 
-        public IEnumerable<CommandSetList> Enumerate()
+        public IEnumerable<CommandSetList> EnumerateCommandSetList()
         {
             return null;
+        }
+
+        public override IEnumerable<DirectoryInfo> Directories
+        {
+            get
+            {
+                foreach (var searcher in Searchers)
+                {
+                    foreach (var dirInfo in searcher.Directories)
+                    {
+                        yield return dirInfo;
+                    }
+                }
+            }
         }
 
         public List<ISearcher> Searchers { get; private set; }
